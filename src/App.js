f1be9eb0f35659c1990450092b0e129aba4751da
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2024-06-07 09:48:12 +0530

    Defining route definitions with createRoutesFromElements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import { Home } from "./components/Home";
+import { Products } from "./components/Products";
+
+// * Another way of creating route definitions instead of object way.
+const routeObject = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<Home />} />
+    <Route path="/products" element={<Products />} />
+  </Route>
+);
 
 
-const router = createBrowserRouter([{
-  path: '/', element: <Home />
-}])
+const router = createBrowserRouter(
+  // [
+  //   { path: '/', element: <Home /> },
+  //   {path : '/products', element: <Products />}
+  // ]
+  routeObject
+);
 
 function App() {
   return (
